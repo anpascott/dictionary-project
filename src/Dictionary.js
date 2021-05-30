64a2f0b83c12f8ec0948b30aaac1ba6ea39e79f0
anpascott <anpascott@gmail.com>
2021-05-30 18:20:15 -0500

    Added word results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,15 @@
 import React, { useState } from "react";
-import Axios from "axios";
 import "./Dictionary.css";
 import axios from "axios";
-import "./WordSearch.js";
+import Results from "./Results.js";
 
 export default function Dictionary () {
 
 let [keyword, setKeyword] = useState("");
-let [wordInfo, setWordInfo] = useState( { ready:false } );
+let [results, setResults] = useState(null);
 
     function handleResponse (response){
-      setWordInfo({
-      ready: true,
-      // word: response.data.word,
-      // phonetics: response.data.phonetics[0].text,
-      // audio: response.data.phonetics[0].audio,
-    });
+      setResults(response.data[0]);
     }
 
   function search (event){
@@ -25,10 +19,8 @@ let [wordInfo, setWordInfo] = useState( { ready:false } );
   }
 
   function updateKeyword (event) {
-  setKeyword(event.target.value);
-    
+  setKeyword(event.target.value);   
   }
-  if (wordInfo.ready) {
     return (
      <div className="Dictionary">
 <h1 className="title">
@@ -46,30 +38,7 @@ Dictionary
   </div>
   </div>
   </div>
- <h2> Searching for {keyword} </h2>
- {/* <WordSearch info={wordInfo}/> */}
+ <Results results={results}/>
    </div>  
     );
-  } 
-  else {
-  return (
-  <div className="Dictionary">
-<h1 className="title">
-False
-  </h1>
-< div className="search-engine">
-  <div className="row row-cols-lg-auto g-3 align-items-center">
-  <div className="col-md-9">
-  <form onSubmit={search}>
-    <input type="Search" autoFocus={true} placeholder="Type a word" onChange={updateKeyword}/>
-  </form>
-  </div>
-    <div className="col-md-3">
-  <button type="button" className="btn btn-light" onClick={search}>Search</button>
-  </div>
-  </div>
-  </div>
-   </div>
-  );
-}
 }
